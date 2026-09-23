0fa04b71e8a2ea43c73215064ff9d10432230a5b
Elmir <elmirisgndrov041@gmail.com>
2026-09-23 10:58:41 +0400

    correction

--- app/src/main/java/com/example/mymoviesapp/retrofit/model/MoviesResponse.kt
+++ app/src/main/java/com/example/mymoviesapp/retrofit/model/MoviesResponse.kt
@@ -1,22 +1,10 @@
 package com.example.mymoviesapp.retrofit.model
 
 data class MoviesResponse(
-    //val page: Int,
     val results: List<Movie>
-    //val total_pages: Int,
-    //val total_results: Int
 )
 
 data class Movie(
-    //val adult: Boolean,
-    //val backdrop_path: String,
-    //val genre_ids: List<Int>,
-    //val original_language: String,
-    //val original_title: String,
-    //val popularity: Double,
-    //val video: Boolean,
-    //val vote_average: Double,
-    //val vote_count: Int,
     val id: Int,
     val overview: String,
     val poster_path: String?,
--- app/src/main/java/com/example/mymoviesapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/mymoviesapp/ui/home/HomeViewModel.kt
@@ -22,13 +22,6 @@ class HomeViewModel @Inject constructor(private val repository: Repository) : Vi
     private val _topRatedMovieResult = MutableLiveData<MoviesResponse?>()
     val topRatedMovieResult: LiveData<MoviesResponse?> = _topRatedMovieResult
 
- /* val topRatedMovieResult = MutableLiveData<MoviesResponse?>() -- Eger pramoy bele etseydik Fragment və Activity bu dəyişəni dəyişdirə bilərdi.
-    Buda, arxitektura üçün təhlükəlidir! View yalnız məlumatı göstərməli, onu dəyişməməlidir.
-    Bu yanaşma Encapsulation (Qablaşdırma) prinsipi ilə daha təhlükəsiz və strukturlaşdırılmış kod yazmağa kömək edir.
-
-    🔹 ViewModel məlumatı dəyişir.
-    🔹 View yalnız oxuyur, dəyişdirə bilmir.
-    🔹 Kod təmiz və asan idarə olunan olur 🚀*/
 
     private val _nowPlayingMoviesResult = MutableLiveData<MoviesResponse?>()
     val nowPlayingMoviesResult: LiveData<MoviesResponse?> = _nowPlayingMoviesResult
